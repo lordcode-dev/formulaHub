077017c0fe46b674474f6fb2e223c0d0c8ee5efd
lordcode-dev <mrbeast765389@gmail.com>
2025-08-25 17:52:55 +0300

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+function SearchBar({ onSearch }) {
+  return (
+    <input
+      type="text"
+      placeholder="Search formulas..."
+      onChange={(e) => onSearch(e.target.value)}
+      className="search-bar"
+    />
+  );
+}
+
+export default SearchBar;
 
