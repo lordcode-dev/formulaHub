441b535484011491b48f5e1b8beda0b4562bf6be
lordcode-dev <mrbeast765389@gmail.com>
2025-08-25 17:45:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import FormulaList from './components/FormulaList';
+import FormulaDetail from './components/FormulaDetail';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<FormulaList />} />
+            <Route path="/formula/:id" element={<FormulaDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
