00d2ddf3ac3f25a1a927931d7cc0088644ba980a
lordcode-dev <mrbeast765389@gmail.com>
2025-08-25 17:52:10 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">FormulaHub</Link>
+        <div className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
